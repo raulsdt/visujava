1258746f2a4c369146dc8bfd9c39c071ee2652dd
Jose Manuel <serrano5510@hotmail.com>
2011-11-14 13:44:09 +0100

    Clave Vector implementada - Clase RectaPrm hay que implementarla

--- src/javaGeom/RectaPrm.java
+++ src/javaGeom/RectaPrm.java
@@ -6,22 +6,82 @@ import math.geom2d.Point2D;
 
 public class RectaPrm {
    StraightLine2D r;
-    
+   
+   /**
+    * Constructor 
+    * @param v1
+    * @param v2 
+    */
    public RectaPrm (Vector v1, Vector v2){
        Point2D p1 = new Point2D (v1.leex(),v1.leey());
        Point2D p2 = new Point2D (v2.leex(),v2.leey());
        r = new StraightLine2D (p1,p2);
    }
-
+   
+   /**
+    * Constructor
+    * @param p1
+    * @param p2 
+    */
    public RectaPrm (Punto p1, Punto p2){
        Point2D pp1 = new Point2D (p1.leex(),p1.leey());
        Point2D pp2 = new Point2D (p2.leex(),p2.leey());
        r = new StraightLine2D (pp1,pp2);
    }
     
+   /**
+    * Constructor
+    * @param r1
+    * @return 
+    */
    public Punto intersecta (RectaPrm r1){
        Point2D p = r.getIntersection(r1.r);
        Punto v = new Punto (p.getX(),p.getY());
        return v;
    }
+   
+   //-------------------- OPERACIONES
+   
+   /**
+    * Dice si un punto está contenido en la recta
+    * @param p
+    * @return 
+    */
+   public boolean contiene(Punto p){
+       return r.contains(p.leex(), p.leey());
+   }
+   
+   /**
+    * Recta paralela a una distancia dada de la recta this
+    * @param x
+    * @return 
+    */
+   public RectaPrm paralelaDistancia(float x){
+       //IMPLEMENTAR
+       return null;
+   }
+   
+   /**
+    * Calcular una nueva recta perpendicular a this en un punto
+    */
+   public RectaPrm perpendicularPunto(Punto p){
+       //IMPLEMENTAR
+       return null;
+   }
+   
+   /**
+    * Calcula la distancia de la recta (this) al punto
+    */
+   public double distanciaPunto(Punto p){
+       //IMPLEMENTAR
+       return 0;
+   }
+   
+   /**
+    * Escribe en pantalla una recta
+    */
+   public void out(){
+       
+   }
+   
 }
--- src/javaGeom/Vector.java
+++ src/javaGeom/Vector.java
@@ -6,14 +6,10 @@ package javaGeom;
 
 import math.geom2d.Vector2D;
 
-/**
- *
- * @author lidia
- */
 
 public class Vector {
   Vector2D v;  
-
+  
     private Vector (Vector2D vr){
         v = vr.clone();
     }    
@@ -43,8 +39,7 @@ public class Vector {
     public Vector (Vector v1, Vector v2) {
         v = new Vector2D (v2.v.getX()-v1.v.getX(), v2.v.getY()-v1.v.getY());
     }
-
-   
+    
     /** Devuelve coordenada x */
     public double leex()  {
         return v.getX();
@@ -55,51 +50,92 @@ public class Vector {
         return v.getY();
     }
 
+   //---------------------------- OPERACIONES
     
-    /** Obtiene la longitud del vector actual. */
+    /** 
+     * Obtiene la longitud del vector actual
+     * @return 
+     */
     public double longitud () {
         return Math.sqrt(Math.pow(v.getX(),2) + Math.pow(v.getY(),2));
     }
-
+    
+    /**
+     * Longitud del vector al cuadrado
+     * @return 
+     */
+    public double longitud2(){
+        return Math.pow(longitud(), 2);
+    }
+    
+    /**
+     * Vector perpendicualar
+     * @return
+     */
+    public Vector perpendicular(){
+        return new Vector(leey() * -1,leex());
+    } 
+    
+    /**
+     * Angulo que forma el vector
+     * @return 
+     */
     public double angulo (){
        return v.getAngle();   
     }    
 
-    /** Suma al vector actual el vector v, devolviendo un nuevo vector con 
-     el resultado de la suma. */
+    /** 
+     * Suma al vector actual el vector v, devolviendo un nuevo vector con el resultado de la suma
+     * @return 
+     */
     public Vector suma (Vector v1) {
         return new Vector(v.plus(v1.v));
     }
 
-    /** Resta al vector actual el vector v, devolviendo un nuevo vector con 
-     el resultado de la resta. */
+    /** 
+     * Resta al vector actual el vector v, devolviendo un nuevo vector con el resultado de la resta
+     * @return 
+     */
     public Vector resta (Vector v1) {
         return new Vector(v.minus(v1.v));
     }
 
-    /** Obtiene un nuevo vector resultado del producto del vector actual por un escalar*/
+    /** 
+     * Obtiene un nuevo vector resultado del producto del vector actual por un escalar
+     * @return 
+     */
     public Vector prodEscalar (double t){
         return new Vector(v.times(t));
     }    
 
     
-    /** Obtiene el producto escalar del vector actual con el vector v. */
+    /** Obtiene el producto escalar del vector actual con el vector v.
+     * @return 
+     */
     public double dot (Vector v1) {
         return v.dot(v1.v);
     }
     
-    /** Obtiene el producto cruzado del vector actual con el vector v. */
+    /** 
+     * Obtiene el producto cruzado del vector actual con el vector v
+     * @return 
+     */
     public double kross (Vector v1) {
         return v.cross(v1.v);
     }
 
         
-    /** Dice si el vector actual es ortogonal  */
+    /** 
+     * Dice si el vector actual es ortogonal
+     * @return 
+     */
     public boolean ortogonal (Vector v1) {
         return v.isOrthogonal(v1.v);
     }
 
-        /** Muestra por pantalla la información del vector actual. */
+    /** 
+     * Muestra por pantalla la información del vector actual. 
+     */
     public void out () {
         String salida = v.toString();
         System.out.print (salida);
